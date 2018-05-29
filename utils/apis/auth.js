04e8006f3ed404e137cfa993df8905aa0c998ebf
Dani Lesmiadi <dani@foodgasm.id>
2018-05-29 23:39:14 +0700

    Fixing API_URL

--- utils/apis/auth.js
+++ utils/apis/auth.js
@@ -1,6 +1,6 @@
 import axios from "axios"
 
-axios.defaults.baseURL = process.env.AUTH_URL
+axios.defaults.baseURL = process.env.API_URL
 // axios.defaults.headers.common['Authorization'] = AUTH_TOKEN;
 axios.defaults.headers.post["Content-Type"] = "application/json"
 
